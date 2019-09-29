e942f12aeb899fbf19880af58de20b7f6598052c
William Famor <jaredfamor@gmail.com>
2019-09-29 15:42:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-    array = ["wow", "I", "am", "really", "learning", "arrays!"]array [1]
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
     array [0]
 end
 
