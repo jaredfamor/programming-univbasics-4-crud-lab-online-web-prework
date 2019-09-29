273ae0f2cd6ca8a600234543bcefb72b772e04de
William Famor <jaredfamor@gmail.com>
2019-09-29 15:24:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
    array = ["I", "am", "really", "learning", "arrays!"]
- Arrays!_array = array.pop
+ arrays!_array = array.pop
    p  array
    p arrays!_array
 end
