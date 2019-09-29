e5d4062d5f18a11c6d5c7e97f0c0a2c5296408fd
William Famor <jaredfamor@gmail.com>
2019-09-29 15:44:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -48,6 +48,6 @@ end
 
 def update_element_from_index(array, index_number, element)
     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-        array = ["wow", "I", "am", "really", "learning", "arrays!"]
+        array [4]
     
 end
